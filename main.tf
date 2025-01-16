24f2f6536780c91de3bf110d1286bcf6fcd91acb
durrail <durrailg@gmail.com>
2025-01-15 20:00:47 -0600

    Use our HCP Terraform organization

--- main.tf
+++ main.tf
@@ -11,7 +11,7 @@ provider "aws" {
 terraform {
 
   cloud {
-    organization = "FlemingFriday"
+    organization = "tfe_github_commands"
 
     workspaces {
       name = "learn-terraform-github-actions"
